5491a0340e5d2d0d374e5426a9b9d038b75d6516
jeosafa eller <jeosafaeller@gmail.com>
2023-08-13 10:28:06 -0300

    game components feat!

---
+++ src/componets/Game.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { Colors } from "./styles/colors"
+
+function Game():JSX.Element {
+  return (
+    <SafeAreaView>
+        
+
+
+    </SafeAreaView>
+  )
+}
+
+export default Game
+
+const style = StyleSheet.create({
+    container:{
+        flex:1, 
+        backgroundColor:Colors.background,
+        alignItems:'center',
+        justifyContent:'center'
+    }
+})
