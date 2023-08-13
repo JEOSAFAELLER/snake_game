013845f17bb2b13046473bae2811f9e126db18a6
jeosafa eller <jeosafaeller@gmail.com>
2023-08-13 19:26:05 -0300

    snake feat!

--- src/componets/Game.tsx
+++ src/componets/Game.tsx
@@ -2,7 +2,8 @@ import * as React from 'react'
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import { Colors } from "./styles/colors"
 import { PanGestureHandler } from 'react-native-gesture-handler';
-import { GestureEventType } from './types/types';
+import { Coordinate, Direction, GestureEventType } from './types/types';
+import Snake from './Snake';
 
 const SNAKE_INITIAL_POSITION = [{ x: 5, y:5 }];
 const FOOD_INITIAL_POSITION = { x: 5, y: 20 };
@@ -15,7 +16,48 @@ const SCORE_INCREMENT = 10;
 
 function Game():JSX.Element {
 
-    
+    const [direction, setDirection] = React.useState<Direction>(Direction.Right);  
+    const [snake, setSnake] = React.useState<Coordinate[]>(SNAKE_INITIAL_POSITION);
+    const [food, setFood] = React.useState<Coordinate>(FOOD_INITIAL_POSITION);
+    const [isGameOver, setIsGameOver] = React.useState<boolean>(false);
+    const[isPaused, setIsPaused] = React.useState<boolean>(false);
+
+    React.useEffect(() => {
+        if(!isGameOver) {
+           
+          const intervalId = setInterval(() => {!isPaused &&  moveSnake()}, MOVE_INTERVAL)
+            
+            return () => clearInterval(intervalId);
+        }
+    } , [snake, isGameOver, isPaused]);
+
+    const moveSnake = () => {
+        const snakeHead = snake[0];
+        const newHead = { ...snakeHead}//creating a copy
+
+        //game over
+
+        switch (direction){
+            case Direction.Up:
+                newHead.y -= 1;
+                break;
+            case Direction.Down:
+                newHead.y += 1;
+                break;
+            case Direction.Left:
+                newHead.x -= 1;
+                break;
+            case Direction.Right:
+                newHead.x += 1;
+                break;
+            default:
+                break;
+
+        }
+
+        setSnake([newHead, ...snake]);
+    }    
+
  const handleGesture = (event:GestureEventType) => {
     const { translationX, translationY } = event.nativeEvent;
     console.log(translationX, translationY);
@@ -23,19 +65,19 @@ function Game():JSX.Element {
     if(Math.abs(translationX) > Math.abs(translationY)) {
         if(translationX > 0) {
             //moving right
-            console.log('Right Swipe');
+            setDirection(Direction.Right);
         }else{
             //moving left
-            console.log('Left Swipe');
+            setDirection(Direction.Left);
         }
 
     }else{
         if(translationY > 0) {
             //down
-            console.log('Down Swipe');
+            setDirection(Direction.Down);
         }else{
             //up
-            console.log('Up Swipe');
+            setDirection(Direction.Up);
         }
     }
 
@@ -46,7 +88,11 @@ function Game():JSX.Element {
     return (
 
     <PanGestureHandler onGestureEvent={handleGesture}>
-        <SafeAreaView  style={style.container}></SafeAreaView>
+        <SafeAreaView  style={style.container}>
+            <View style={style.boundaries}>
+                <Snake snake={snake}/>
+            </View>
+        </SafeAreaView>
 
     </PanGestureHandler>
     
@@ -59,7 +105,15 @@ const style = StyleSheet.create({
     container:{
         flex:1, 
         backgroundColor:Colors.primary,
-        alignItems:'center',
-        justifyContent:'center'
+       
+    },
+    boundaries:{
+        flex:1,
+        borderColor: Colors.primary,
+        borderWidth: 12,
+        borderBottomLeftRadius: 30,
+        borderBottomRightRadius: 30,
+        backgroundColor: Colors.background,
+
     }
 })
